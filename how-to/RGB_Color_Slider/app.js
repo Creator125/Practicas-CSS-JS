1c23a4582b725b67bb2e1c54d76e123db2b81bd6
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-12-19 07:32:47 -0500

    nueva practica "how to"

---
+++ how-to/RGB_Color_Slider/app.js
@@ -0,0 +1,34 @@
+const result = document.querySelector(".result input");
+const copyBtn = document.getElementById("copy-btn");
+const sliders = document.querySelectorAll(".wrapper input[type='range']");
+const rColor = document.getElementById("red");
+const gColor = document.getElementById("green");
+const bColor = document.getElementById("blue");
+
+//Función para generar colores
+let generateColor = () => {
+    let rColorValue = rColor.value;
+    let gColorValue = gColor.value;
+    let bColorValue = bColor.value;
+
+    let finalColor = `rgb(${rColorValue}, ${gColorValue}, ${bColorValue})`;
+    result.value = finalColor;
+
+    document.body.style.backgroundColor = finalColor;
+}
+
+//Función para copiar colores
+let copyColorCode = () => {
+    result.select();
+    document.execCommand("copy")
+    copyBtn.innerText = "Copiado";
+    setTimeout(() => {
+        copyBtn.innerText = "Copiar código de color"
+    }, 1000)
+}
+
+sliders.forEach((inp) => {
+    inp.addEventListener("input", generateColor);
+});
+window.addEventListener("load",generateColor);
+copyBtn.addEventListener("click", copyColorCode);
